3df4e9d0e83a7c99e72d0cb893ea1d60d62f3d69
Saqib Masoodi <masudisaqib1@gmail.com>
2025-06-22 20:54:50 +0530

    interfaces and sub interfaces

---
+++ Interface.java
@@ -0,0 +1,18 @@
+interface A {
+    void show();
+}
+interface B1 extends A {
+    void show2();
+}
+
+class B implements B1 {
+    void show() {
+        System.out.println("Brr Brr patapim");
+    }
+}
+
+class Interface {
+    public static void main(String[] args) {
+        A ob1
+    }
+}
